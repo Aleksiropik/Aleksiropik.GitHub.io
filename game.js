a84ba40d107c235d2d3d675d2101486e6e069de1
Aleksiropik <121820247+Aleksiropik@users.noreply.github.com>
2023-07-30 17:38:25 +0200

    ,kdjnb

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ function drawPaddle() {
   var y = canvas.height - 30;
 
   function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
@@ -30,5 +31,6 @@ function drawPaddle() {
     x += dx;
     y += dy;
   }
+  
   
   setInterval(draw, 10);
