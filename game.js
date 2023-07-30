1f707737244feef219f78d73925e64d516eeb0b5
Aleksiropik <121820247+Aleksiropik@users.noreply.github.com>
2023-07-30 17:21:24 +0200

    gg

--- game.js
+++ game.js
@@ -20,5 +20,7 @@ ctx.arc(50, 50, 10, 0, Math.PI * 2);
 ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
+var x = canvas.width / 2;
+var y = canvas.height - 30;
   }
   setInterval(draw, 10);
